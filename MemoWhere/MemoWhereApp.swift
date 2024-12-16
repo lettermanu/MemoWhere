704e2e811060aca93ecf804212022cc4225face1
Manuela Letteriello <m.letteriello@yahoo.it>
2024-12-16 14:38:57 +0100

    Initial Commit

---
+++ MemoWhere/MemoWhereApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoWhereApp.swift
+//  MemoWhere
+//
+//  Created by Manuela Letteriello on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoWhereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
